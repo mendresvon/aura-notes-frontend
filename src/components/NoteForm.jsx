50fff6a49e3130a6b35d5750a66d0a4f008bfb4b
Von Mendres <mendresvon@gmail.com>
2025-09-03 13:14:08 +0800

    Improved the look and feel of noteform when editing the note

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -29,7 +29,7 @@ const NoteForm = ({ onSave, onCancel, note }) => {
           onChange={(e) => setTitle(e.target.value)}
           required
           autoFocus
-          className="w-full p-3 rounded-md bg-black/40 border border-white/20 placeholder-gray-500 focus:ring-2 focus:ring-purple-500 outline-none transition-all duration-600"
+          className="w-full p-3 rounded-md bg-black/40 border border-white/20 placeholder-gray-500 focus:ring-1 focus:ring-purple-600 outline-none transition-all duration-300 hover:border-purple-800"
         />
       </div>
       <div>
@@ -42,19 +42,19 @@ const NoteForm = ({ onSave, onCancel, note }) => {
           onChange={(e) => setContent(e.target.value)}
           required
           rows="10"
-          className="w-full p-3 rounded-md bg-black/40 border border-white/20 placeholder-gray-500 focus:ring-2 focus:ring-purple-500 outline-none resize-y transition-all duration-600"
+          className="w-full p-3 rounded-md bg-black/40 border border-white/20 placeholder-gray-500 focus:ring-1 focus:ring-purple-600 outline-none resize-y transition-all duration-300 hover:border-purple-800"
         />
       </div>
       <div className="flex justify-end gap-4">
         <button
           type="button"
           onClick={onCancel}
-          className="px-5 py-2 text-gray-400 font-semibold rounded-md hover:bg-white/5 transition-colors">
+          className="px-5 py-2 bg-black/90! text-gray-400 font-semibold rounded-md hover:bg-white/5 hover:text-white transition-colors">
           Cancel
         </button>
         <button
           type="submit"
-          className="px-5 py-2 text-white font-semibold bg-gradient-to-r from-purple-600 to-indigo-600 rounded-md hover:opacity-90">
+          className="px-5 py-2 text-white font-semibold bg-gradient-to-l from-purple-900 to-indigo-900 rounded-md hover:opacity-90 transform hover:-translate-y-1 !transition-transform">
           Save Note
         </button>
       </div>
